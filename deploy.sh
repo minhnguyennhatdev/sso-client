221326abb7945c2f23cd1477001a8d9ca088fdc4
Minh Nguyen <nhatminhnguyen061200@gmail.com>
2024-04-19 15:20:23 +0700

    sso client

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git fetch --all && git reset --hard @{u}
 
 echo "Deplying..."
 
-docker build -t 'sso-client' .
+docker build --tag 'sso-client' .
 docker run -d -p 3001:3001 'sso-client'
 
 echo "Done."
