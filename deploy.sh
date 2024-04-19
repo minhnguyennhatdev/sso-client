6c7f95a77a7d22582679ede87a2473f5e8ce7c75
Minh Nguyen <nhatminhnguyen061200@gmail.com>
2024-04-19 16:28:24 +0700

    fix state null

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ git fetch --all && git reset --hard @{u}
 
 echo "Deplying..."
 
+
+docker stop 'sso-client'
+docker rm 'sso-client'
 docker build --tag 'sso-client' .
-docker run -d -p 3001:3001 'sso-client'
+docker run --name 'sso-client' -d --restart -p 3001:3001 'sso-client'
 
 echo "Done."
